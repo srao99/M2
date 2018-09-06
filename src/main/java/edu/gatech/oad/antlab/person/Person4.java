bddad58bde7a3a2ac293113b262d8ff38e150016
Stuti Rao <Stuti@lawn-143-215-63-20.lawn.gatech.edu>
2018-09-06 17:46:40 -0400

    edited Person4

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Stuti
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,10 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+      for (i = 0; i < input.length; i++) {
+        input[i] = char (((int) input[i]) + 1)
+      }
+      return input;
     }
     
     /**
